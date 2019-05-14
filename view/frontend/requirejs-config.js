1a01386a1ea9044f5abc542727900bb45c22d239
root <root@ip-172-26-13-81.eu-west-1.compute.internal>
2019-05-14 09:57:19 +0200

    update preview and reset added

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,7 @@ var config = {
         '*':{
             PitchPrintAfterSave: 'PitchPrintInc_PitchPrint/js/view/afterSave',
             PitchPrintAfterAddCart: 'PitchPrintInc_PitchPrint/js/view/afterAddCart',
+            PitchPrintOnProductLoad: 'PitchPrintInc_PitchPrint/js/view/onProductLoad',
             // 'Magento_Checkout/template/summary/item/details/thumbnail.html' :
             // 'PitchPrintInc_PitchPrint/template/summary/item/details/thumbnail.html'
         }
--- view/frontend/web/js/view/afterSave.js
+++ view/frontend/web/js/view/afterSave.js
@@ -1,17 +1,79 @@
-window.ppclient.on('project-saved', storeToLocal);
-
-// Get Product Id
-const productSel = document.querySelector('#product_addtocart_form input[name=product]');
-const productId = productSel ? productSel.value : 0;
-
-// Save project ID against product ID in localStorage.
-function storeToLocal(e) {
-    if (!localStorage.hasOwnProperty('pp_w2p_projects')) 
-        localStorage.pp_w2p_projects = JSON.stringify({});
-    if (!productId) 
-        return console.log('Can not find product ID!');
+(function($){
+    window.ppclient.on('project-saved', storeToLocal);
+    
+    // Get Product Id
+    const productSel = document.querySelector('#product_addtocart_form input[name=product]');
+    const productId = productSel ? productSel.value : 0;
+    const ppValEl = document.querySelector('#_pitchprint');
+    
+    // Save project ID against product ID in localStorage.
+    async function storeToLocal(e) {
+        if (!localStorage.hasOwnProperty('pp_w2p_projects')) 
+            localStorage.pp_w2p_projects = JSON.stringify({});
+        if (!productId) 
+            return console.log('Can not find product ID!');
+            
+        let currentLocalStorage = JSON.parse(localStorage.pp_w2p_projects);
+        let _projectData = await getPPInputValue();
+        currentLocalStorage[productId] = JSON.parse(decodeURIComponent(_projectData));
+        localStorage.pp_w2p_projects = JSON.stringify(currentLocalStorage);
         
-    let currentLocalStorage = JSON.parse(localStorage.pp_w2p_projects);
-    currentLocalStorage[productId] = e.data.projectId;
-    localStorage.pp_w2p_projects = JSON.stringify(currentLocalStorage);
-}
+        updatePPPreview();
+    }
+    
+    let attempts = 20;
+    let _interval;
+    
+    function getValueNow(_resolve) {
+        if (ppValEl.value) {
+            clearInterval(_interval)
+            _resolve(ppValEl.value);
+        }
+    }
+    
+    function getPPInputValue() {
+        return new Promise(_res=>{
+            if(!ppValEl.value)
+                _interval = setInterval(_=>getValueNow(_res),500);
+            else _res(ppValEl.value);        
+        });
+    
+    }
+    
+    function updatePPPreview() {
+        $ = jQuery;
+        setTimeout(_=>{
+            const _n = Math.random();
+            const _prev = `https://s3-eu-west-1.amazonaws.com/pitchprint.io/previews/${JSON.parse(localStorage.pp_w2p_projects)[productId].projectId}_1.jpg?
+                         rand=${_n}`;
+            $('.gallery-placeholder').html(`<img src="${_prev}">`);
+            if(!window.ppclient.galleryCentered) centerGal();
+            if(!window.ppclient.resetActive) activateResetButton();
+        },500);
+    }
+    
+    function centerGal() {
+        $('.gallery-placeholder').css('text-align', 'center');
+        window.ppclient.galleryCentered = 1;
+    }
+    
+    let resetAttemps = 50;
+    function activateResetButton() {
+        if (!resetAttemps) return;
+        if (!$('#pp_clear_design_btn').length) {
+            setTimeout(activateResetButton,500);
+            resetAttemps--;
+            return;
+        }
+        $('#pp_clear_design_btn').click(clearPPProject);
+        window.ppclient.resetActive = 1;
+    }
+    
+    function clearPPProject() {
+      if (!productId) {console.log('No Product ID found!');return;}
+      let newLocalS = JSON.parse(localStorage.pp_w2p_projects)
+      delete newLocalS[productId];
+      localStorage.pp_w2p_projects = JSON.stringify((newLocalS));
+      window.location.reload();
+    }
+})(jQuery);
